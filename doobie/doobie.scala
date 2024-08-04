696b80742c84a80c4389f7f21182bfdc3ad49e4a
EstebanMarin <estebanmarin002@gmail.com>
2024-08-03 22:02:24 -0500

    cleanup

--- doobie/doobie.scala
+++ doobie/doobie.scala
@@ -10,26 +10,6 @@ import cats.effect.IO
 import scala.concurrent.ExecutionContext
 import cats.effect.unsafe.implicits.global
 
-// val xa = Transactor.fromDriverManager[IO](
-//   driver = "org.postgresql.Driver",
-//   url = "jdbc:postgresql:world",
-//   user = "postgres",
-//   password = "password",
-//   logHandler = None
-// )
-
-// case class Country(code: String, name: String, population: Long)
-
-// def find(n: String): ConnectionIO[Option[Country]] =
-//   sql"select code, name, population from country where name = $n"
-//     .query[Country]
-//     .option
-
-// def findAll =
-//   sql"select * from country"
-//     .query[String]
-//     .to[List]
-
 object Main extends IOApp.Simple {
 
   val xa = Transactor.fromDriverManager[IO](
